372335e554762af1b742602af38790e057204ad8
Wade <wade.darsow@gmail.com>
2018-09-16 21:17:21 -0500

    added new if statement

--- ydkjs/activity1.js
+++ ydkjs/activity1.js
@@ -22,13 +22,27 @@ function purchPhone() {
 }
 
 function taxCalculation(currentPurchase) {
-    console.log(currentPurchase)
+    // console.log(currentPurchase)
     moneyFormat(Number.parseFloat(((taxRate * currentPurchase) + currentPurchase).toFixed(2)));   
 }
 
 function moneyFormat(arg1) {
-    let formattedPurchPrice = (`$${arg1}`);
-    console.log("Your total current phone and accessory costs including taxes are: " + formattedPurchPrice);
+    let formattedPurchPriceString = (`$${arg1}`);
+    let formattedPurchPrice = arg1;
+    console.log("Your total current phone and accessory costs including taxes are: " + formattedPurchPriceString);
+    console.log(formattedPurchPrice);
+    if(formattedPurchPrice < bankBalance) {
+        console.log("You just purchased a new phone and " + accessoryPurchaseCounter + " new accessories!")
+        bankBalance -= formattedPurchPrice;
+        console.log("Your new bank balance is: " + bankBalance);
+        accessoryPurchaseCounter = 0;
+        phonePurchaseCounter = 0;
+        currentPurchase = 0;
+        formattedPurchPrice = 0;
+        purchPhone();
+    } else {
+        console.log("Sorry, you don't have enough money to purchase the phone and accessories you selected. Your current bank balance is: " + bankBalance);
+    }
 }
 
 purchPhone();
